2d92b86bdd05a1c61de77a64f985c458efb2702a
tiesen243 <ttien56906@gmail.com>
2025-12-31 10:09:10 +0700

    Fix complex number representation in X calculation

--- bai-1/cau_1.m
+++ bai-1/cau_1.m
@@ -2,7 +2,7 @@ n = -10:11;
 x = 0.6 .^ abs(n);
 k = -500:500;
 w = (pi / 500) * k;
-X = x * (exp(-j * pi / 500)) .^ (n' * k);
+X = x * (exp(-1i * pi / 500)) .^ (n' * k);
 magX = abs(X);
 angX = angle(X);
 
